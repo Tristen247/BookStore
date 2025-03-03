2668577dc6a4b1761e9f3689d7d12901dc4c8333
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2025-03-03 00:47:27 -0500

    Scaffolded DB, created basic functions, and unit tests.

---
+++ BookStore/BookStoreBasicFunctions.cs
@@ -0,0 +1,45 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class BookStoreBasicFunctions
+    {
+
+        // Return all books
+        public static List<Book> GetAllBooks()
+        {
+            using (var dbContext = new Se407BookstoreContext())
+            {
+                return dbContext.Books.ToList();
+            }
+        }
+
+        // Return book by title
+        public static List<Book> GetBookByTitle(string title)
+        {
+            using (var dbContext = new Se407BookstoreContext())
+            {
+                return dbContext.Books
+                    .Where(b => b.BookTitle == title)
+                    .ToList();
+            }
+        }
+
+        //Get by author last
+        public static List<Book> GetBookByAuthorLastName(string authorLastName)
+        {
+            using (var dbContext = new Se407BookstoreContext())
+            {
+                return dbContext.Books
+                    .Where(b => b.Author.AuthorLast == authorLastName)
+                    .ToList();
+            }
+        }
+
+    }
+}
---
+++ BookStoreTest/BookStoreBasicFunctionsTest.cs
@@ -0,0 +1,34 @@
+﻿using BookStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookStoreTest
+{
+    public class BookStoreBasicFunctionsTest
+    {
+        [Fact]
+        public void GetAllBooks()
+        {
+            var result = BookStoreBasicFunctions.GetAllBooks();
+            Assert.True(result.Count > 0);
+        }
+
+        [Fact]
+        public void GetBookByTitle()
+        {
+            var result = BookStoreBasicFunctions.GetBookByTitle("Canterbury Tales");
+            Assert.True(result.Count == 1);
+        }
+
+        [Fact]
+        public void GetBookByAuthorLast()
+        {
+            var result = BookStoreBasicFunctions.GetBookByAuthorLastName("Chaucer");
+            Assert.True(result.Count == 2);
+        }
+    }
+}
